dfdc1689e0e7e8a0443b501851a62acbf5d771a4
ShadyFox2004 <amboudreau.amb@gmail.com>
2022-03-05 13:01:12 -0500

    Test VecteurFormes Fixes #4

---
+++ src/tests/VecteurFormeTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import org.junit.Before;
+
+/**
+ * Classe de test des vecteur:
+ * Spécification de l'api.
+ * @author Antoine-Matis Boudreau
+ */
+public class VecteurFormeTest {
+    @Before
+    /**
+     * 
+     */
+    public void setUp() {
+        
+    }
+
+}
