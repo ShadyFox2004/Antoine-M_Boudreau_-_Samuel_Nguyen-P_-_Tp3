80486ed3a024398129b944e21a0e1cf04be2f330
ShadyFox2004 <amboudreau.amb@gmail.com>
2022-03-05 13:37:18 -0500

    Also fixed Triangle.java

--- src/tests/VecteurFormeTest.java
+++ src/tests/VecteurFormeTest.java
@@ -1,19 +1,45 @@
 package tests;
 
 import org.junit.Before;
+import org.junit.Test;
 
 /**
  * Classe de test des vecteur:
- * Spécification de l'api.
+ * Spécification de l'api public.
  * @author Antoine-Matis Boudreau
  */
 public class VecteurFormeTest {
     @Before
     /**
-     * 
+     * Test du constructeur.
      */
     public void setUp() {
-        
+        // TODO Determiner les specification de l'object
     }
 
+    @Test
+    public void getVecteur() {
+      // TODO Doit donner une array 
+    }
+
+    @Test
+    public void melanger() {
+        // TODO Verifier si melanger est asser impredictible.
+    }
+
+    @Test
+    public void remplir() {
+        // TODO Doit permetre de remplir le vecteur avec les forme manquante
+    }
+
+    @Test
+    public void toString() {
+        // TODO Doit retourner un chaine string selon les standard etablis
+        // TODO FAIL si la chaine retourner est null //Permet d'idiquer si non implementer
+    }
+
+    @Test
+    public void trier() {
+        // TODO Doit trier les objects selon les standart etablis
+    }
 }
